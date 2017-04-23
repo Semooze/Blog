6051ac8aba4fe0e7a46aa2c283ff93dc1f7803dc
Semooze <cloudtimewater@gmail.com>
2017-04-23 12:55:47 +0000

    Decorate view

--- blog/resources/views/posts/index.blade.php
+++ blog/resources/views/posts/index.blade.php
@@ -1,14 +1,26 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}">
     <head>
-
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
     </head>
     <body>
-        <h3>My Blog</h3>
-        <ul>
+        <div class="container">
+            <div class="mb-5">
+                <h3 >My Blog</h3>
+            </div>
+    
             @foreach ($posts as $post)
-                <li>{{ $post }}</li>
+                <div class="card mb-5">
+                    <div class="card-header">
+                        {{ $post->title }}
+                    </div>
+                    <div class="card-block">
+                        {{ $post->body }}
+                    </div>
+                </div>
             @endforeach
-        </ul>
+
+        <a href="/posts/create" class="btn btn-primary" role="button" aria-disabled="true">Create new post</a>
+        </div>
     </body>
 </html>
