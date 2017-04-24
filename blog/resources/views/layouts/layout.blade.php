d737e631f2d8fa613dd154cf06d923af5c271824
Semooze <cloudtimewater@gmail.com>
2017-04-24 16:57:38 +0000

    Decorate display for markdown

--- blog/resources/views/layouts/layout.blade.php
+++ blog/resources/views/layouts/layout.blade.php
@@ -3,6 +3,7 @@
     <head>
         <title>My Blog</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+        <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
         @include('layouts.header')
