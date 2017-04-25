63861ff8f50b99dd1c75cf03b0abe2e4ddd48879
semooze <cloudtimewater@gmail.com>
2017-04-25 14:05:21 +0000

    Decorate layout of individual post page

--- blog/resources/views/posts/show.blade.php
+++ blog/resources/views/posts/show.blade.php
@@ -3,9 +3,15 @@
 @section('content')
 
     <div class="container">
-        <h2>{{ $post->title }}</h2>
-        <p>@markdown($post->body)</p>
-        <a href="/" class="btn btn-primary" role="button" aria-disabled="true">Back to homepage</a>
+        <div class="card">
+            <div class="card-header">
+                <h2>{{ $post->title }}</h2>
+            </div>
+            <div class="card-block">
+                <p>@markdown($post->body)</p>
+                <a href="/" class="btn btn-primary" role="button" aria-disabled="true">Back to homepage</a>
+            </div>
+        </div>
     </div>
 
 @endsection
